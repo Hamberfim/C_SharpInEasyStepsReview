2af0a10fcb8a0ec3e93be9e47880cbc59aac3410
Anthony <adhamlingithub@gmail.com>
2022-03-22 14:52:41 -0500

    user input and covert string to double

--- 03_ReadingInput/Program.cs
+++ 03_ReadingInput/Program.cs
@@ -6,12 +6,22 @@ namespace _03_ReadingInput
     {
         static void Main(string[] args)
         {
-            Console.Title = "Read User Input";
+            Console.Title = "Read and use User Input";
 
             Console.WriteLine("Please Enter Your First Name: ");
             string fName = Console.ReadLine();
 
             Console.WriteLine("\n" + "Welcome " + fName + " to C# in Visual Studo!");
+
+            Console.WriteLine("\nLet's do some conversion. When you provide input it is captured as a string data type");
+
+            Console.WriteLine("Please enter a number: ");
+
+            // convert user input, whcih is a string and convert it to a double data type
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter another number: ");
+            double sum = number + Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("These two numbers have been converted from string to a double data type so they can be added together. \nTotal equals: " + sum);
         }
     }
 }
