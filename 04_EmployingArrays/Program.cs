9cdece6e668085d8b369ef14812963c0f991f87f
Anthony <adhamlingithub@gmail.com>
2022-03-22 17:16:36 -0500

    additional arrays

--- 04_EmployingArrays/Program.cs
+++ 04_EmployingArrays/Program.cs
@@ -18,17 +18,28 @@ namespace _04_EmployingArrays
             cars[4] = "Nissan";
 
             Console.WriteLine("Cars:");
+            Console.WriteLine("My favorite car is a " + cars[1]);
             foreach (var item in cars)
             {
                 Console.WriteLine(item);
             }
 
             Console.WriteLine("\nBoat Types:");
+            
             string[] boatTypes = { "row", "sail", "motorized" };
+            Console.WriteLine("My favorite boat type is a " + boatTypes[2] + " boat.");
+
             foreach (var item in boatTypes)
             {
                 Console.WriteLine(item);
             }
+
+            // 2-dimentional array - 2 columns, 3 rows (column one first) (column two second)
+            int[,] coordinates = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            Console.WriteLine("X0,Y0 is a " + coordinates[0, 0]);
+            Console.WriteLine("X1,Y1 is a " + coordinates[1, 1]);
+            Console.WriteLine("X0,Y2 is a " + coordinates[0, 2]);
+
         }
     }
 }
