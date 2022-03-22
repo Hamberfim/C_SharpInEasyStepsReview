79162ba29c13c9b31ea5430ce31297f866564b45
Anthony <adhamlingithub@gmail.com>
2022-03-22 14:37:08 -0500

    read user input

---
+++ 03_ReadingInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03_ReadingInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Read User Input";
+
+            Console.WriteLine("Please Enter Your First Name: ");
+            string fName = Console.ReadLine();
+
+            Console.WriteLine("\n" + "Welcome " + fName + " to C# in Visual Studo!");
+        }
+    }
+}
