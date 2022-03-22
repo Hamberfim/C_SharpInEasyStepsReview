bf671719cd41ee897729bdaafc79d1a900efc7db
Anthony <adhamlingithub@gmail.com>
2022-03-22 11:08:39 -0500

    data types

---
+++ 02_CreatingVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02_CreatingVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Creating Variables";
+
+            char letterA;  // declared
+            letterA = 'A';  // initialized
+            Console.WriteLine("char data type: " + letterA);  // output
+
+            int number = 100;
+            Console.WriteLine("int data type: " + number);
+
+            float bodyTemp = 98.6f;
+            Console.WriteLine("floating point data type: " + bodyTemp);
+
+
+
+        }
+    }
+}
