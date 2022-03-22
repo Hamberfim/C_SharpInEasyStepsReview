c4fda75c7a6b03b214f9acfdc5a856731f8ac7e2
Anthony <adhamlingithub@gmail.com>
2022-03-22 17:07:26 -0500

    additional arrays

--- 04_EmployingArrays/Program.cs
+++ 04_EmployingArrays/Program.cs
@@ -7,9 +7,28 @@ namespace _04_EmployingArrays
         static void Main(string[] args)
         {
             Console.Title = "Employing Arrays Arrays";
-            
 
+            string[] cars = new string[5];  // declared
 
+            // add and element to the array - initialized
+            cars[0] = "BMW";
+            cars[1] = "Fiat";
+            cars[2] = "Ford";
+            cars[3] = "GMC";
+            cars[4] = "Nissan";
+
+            Console.WriteLine("Cars:");
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nBoat Types:");
+            string[] boatTypes = { "row", "sail", "motorized" };
+            foreach (var item in boatTypes)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
