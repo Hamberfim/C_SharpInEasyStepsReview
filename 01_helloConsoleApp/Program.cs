2eaf3dfc59b47cd88a3a85c4dd0f11f39dea5320
Anthony <adhamlingithub@gmail.com>
2022-03-22 09:15:56 -0500

    hello world comments

--- 01_helloConsoleApp/Program.cs
+++ 01_helloConsoleApp/Program.cs
@@ -7,8 +7,8 @@ namespace _01_helloConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Press any key to continue...");  // not necessary with current IDE run config
-            Console.ReadKey();  // not necessary with current IDE run config
+            // Console.WriteLine("Press any key to continue...");  // not necessary with current IDE run config
+            // Console.ReadKey();  // not necessary with current IDE run config
             /* 
              To automatically close the console when debugging stops, 
              enable Tools->Options->Debugging->Automatically close the console when debugging stops.
