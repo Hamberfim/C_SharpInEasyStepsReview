7f0d0082f8188dfe1e47e65f0bcb78dd479f14d0
Anthony <adhamlingithub@gmail.com>
2022-03-23 12:35:44 -0500

    casting data types

---
+++ 06_FixingConstants/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06_FixingConstants
+{
+    class Program
+    {
+        enum Days { Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday};
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Fixing Constants";
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
