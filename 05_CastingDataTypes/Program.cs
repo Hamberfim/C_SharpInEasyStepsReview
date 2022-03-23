cc08474682245cb123903e6773644167ef1e1cea
Anthony <adhamlingithub@gmail.com>
2022-03-23 11:25:04 -0500

    casting data types

--- 05_CastingDataTypes/Program.cs
+++ 05_CastingDataTypes/Program.cs
@@ -34,6 +34,22 @@ namespace _05_CastingDataTypes
 
             double numSum2 = (double)11 / num2;  // Explicit cast
             Console.WriteLine($"Explicit: 11 / {num2} = {numSum2}");
+            double numSum3 = 11 / num2;  // Truncated cast - no cast
+            Console.WriteLine($"Truncated: 11 / {num2} = {numSum3}");
+
+            Console.WriteLine("Cast a numerical ASCII values to a char.");
+            char letterT = (char)84;  // T explicit casting
+            char letterO = (char)79;  // O explicit casting
+            char letterN = (char)78;  // N explicit casting
+            char letterY = (char)89;  // Y explicit casting
+            Console.WriteLine($"ASCII values of 84, 79, 78, 89 with explicit casting and combined = {letterT}{letterO}{letterN}{letterY}");
+
+            Console.WriteLine("Cast ASCII values back to their numerical values.");
+            int asciiT = (int)'T';  // 84 explicit casting
+            int asciiO = (int)'O';  // 79 explicit casting
+            int asciiN = (int)'N';  // 78 explicit casting
+            int asciiY = (int)'Y';  // 89 explicit casting
+            Console.WriteLine($"ASCII values of T, O, N, Y with explicit casting  = {asciiT}, {asciiO}, {asciiN}, {asciiY}");
 
         }
     }
