2215b0c5cd0975c84642ef1d31e92bc319d80cf2
Anthony <adhamlingithub@gmail.com>
2022-03-24 11:14:29 -0500

    arithmetic

---
+++ _07_Arithmetic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07_Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Arithmetic";
+            int ten = 10;
+            int four = 4;
+
+            Console.WriteLine($"Addition: {ten} + {four} = " + (ten + four));
+            Console.WriteLine($"Substraction: {ten} - {four} = " + (ten - four));
+            Console.WriteLine($"");
+            Console.WriteLine($"");
+            Console.WriteLine($"");
+            Console.WriteLine($"");
+            Console.WriteLine($"");
+            Console.WriteLine($"");
+            Console.WriteLine($"");
+        }
+    }
+}
