09b17b011fb2b6c26be1938949ce9b5cc3f75c6f
Anthony <adhamlingithub@gmail.com>
2022-03-25 10:31:33 -0500

    branching logic

---
+++ _08_IfBranchingLogic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _08_IfBranchingLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ternary logic";
+
+            // decare and intialize
+            int negNumericFlag = 0;
+            int posNumericFlag = 1;
+            bool negFlag = false;
+            bool posFlag = true;
+
+            // Ternary operation logic
+            string 
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
