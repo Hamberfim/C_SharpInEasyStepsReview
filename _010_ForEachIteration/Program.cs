eb69157a38d1de30f62a20809c8e810bf85fb553
Anthony <adhamlingithub@gmail.com>
2022-03-26 11:59:25 -0500

    Foreach Iteration

---
+++ _010_ForEachIteration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _010_ForEachIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Foreach iteration";
+            // declare and initialize
+            // syntax | foreach(data-type variable name in array-name/collection) {statements}
+            string[] babyNames = new string[10] { "Liam", "Olivia", "Noah", "Emma", "Oliver", "Ava", "Elijah", "Charlotte", "William", "Sophia" };
+
+
+        }
+    }
+}
