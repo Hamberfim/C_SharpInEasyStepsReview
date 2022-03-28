47e9813f78c9af8fb8057a5d80da20a357df6acf
Anthony <adhamlingithub@gmail.com>
2022-03-28 12:14:40 -0500

    Creating Functions

--- _011_functions/Program.cs
+++ _011_functions/Program.cs
@@ -9,6 +9,28 @@ namespace _011_functions
             Console.Title = "Creating Functions - Methods";
 
             // syntax | access-specifier return-data-type method-name (parameters) {statements}
+            static void bodyTempC()
+            {
+                Console.WriteLine("=== Body Temperate ===");
+                Console.WriteLine($"In Centigrade: 37{'\u00B0'}C");  // Windows Alt Code = (char)0176 in ascii OR Unicode escape sequence '\u00B0'
+            }
+
+            static double bodyTempF()  // method returns floating value for body temp in Farenheit
+            {
+                double temp = 98.6;
+                return temp;
+            }
+
+            static int bodyTempK()  // method returns value for body temp in kelvin
+            {
+                int temp = 310;
+                return temp;
+            }
+
+            bodyTempC();
+            Console.WriteLine($"In Farenheit: {bodyTempF()}{'\u00B0'}F");
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
