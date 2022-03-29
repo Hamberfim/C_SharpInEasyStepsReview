f1954d22f1d9d08d61dedbf368849176719e21f9
Anthony <adhamlingithub@gmail.com>
2022-03-29 16:19:18 -0500

    ASPNET core Jokes app

---
+++ _01_JokesWebApp/Models/Joke.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01_JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        // constructor below will be populated automatically 
+        public Joke()
+        {
+            
+        }
+    }
+
+
+
+}
