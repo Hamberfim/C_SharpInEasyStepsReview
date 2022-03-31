a0723ce02b5f50b4138d5f045fcd1d9190cdcd99
Anthony <adhamlingithub@gmail.com>
2022-03-31 11:21:07 -0500

    string manipulation

--- _014_StringManipulation/Program.cs
+++ _014_StringManipulation/Program.cs
@@ -7,10 +7,27 @@ namespace _014_StringManipulation
         static void Main(string[] args)
         {
             Console.Title = "String Manipulation";
+            Console.WriteLine("Please enter some text: ");
+            string userText = Console.ReadLine();
 
+            Console.WriteLine($"You have entered: \n\t{userText}");
+            Console.WriteLine($"Your text character length is: \n\t{userText.Length}");
+
+            // trim and remove word spacing
+            userText = userText.Trim();
+            string userTextNoSpace = userText.Replace(" ", "");
+            Console.WriteLine($"With space trimmed and removed your text character lenght is: \n\t{userTextNoSpace.Length}");
+
+            userText = userText.ToUpper();
+            Console.WriteLine($"Uppercase: \n\t{userText}");
+            userText = userText.ToLower();
+            Console.WriteLine($"Lowercase: \n\t{userText}");
 
 
             Console.WriteLine();  // space in output
+
+
+
         }
     }
 }
