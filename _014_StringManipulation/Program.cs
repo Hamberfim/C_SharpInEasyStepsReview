18967ecaea997f549a8994a317594a49a8fbeece
Anthony <adhamlingithub@gmail.com>
2022-03-31 11:08:57 -0500

    string manipulation

---
+++ _014_StringManipulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _014_StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "String Manipulation";
+
+
+
+            Console.WriteLine();  // space in output
+        }
+    }
+}
