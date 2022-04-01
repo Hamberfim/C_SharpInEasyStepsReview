f12b71ce430afb19466d44a41cdb221ff1dbccc8
Anthony <adhamlingithub@gmail.com>
2022-04-01 11:49:44 -0500

    Copy and swap strings

--- _016_CopySwapStrings/Program.cs
+++ _016_CopySwapStrings/Program.cs
@@ -7,13 +7,15 @@ namespace _016_CopySwapStrings
         static void Main(string[] args)
         {
             Console.Title = "Copy and Swap strings";
+
             string truck1 = "GMC";
             string truck2 = "Dodge Ram";
             string emptyStrVar = "";
-
-            Console.WriteLine($"Original strings: truck1={truck1}, truck2={truck2}, emptyStrVar{emptyStrVar}");
+            Console.WriteLine($"Original strings: truck1={truck1}, truck2={truck2}, emptyStrVar={emptyStrVar}");
 
             Console.WriteLine();  // space in output
+            emptyStrVar = String.Copy(truck2);
+            Console.WriteLine($"Copy string (emptyStrVar = String.Copy(truck2)): truck1={truck1}, truck2={truck2}, emptyStrVar={emptyStrVar}");
         }
     }
 }
