52576c8600f6810d494a46aac7a33b77ac3baef7
Anthony <adhamlingithub@gmail.com>
2022-04-01 11:46:20 -0500

    Copy and swap strings

---
+++ _016_CopySwapStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _016_CopySwapStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Copy and Swap strings";
+            string truck1 = "GMC";
+            string truck2 = "Dodge Ram";
+            string emptyStrVar = "";
+
+
+            Console.WriteLine();  // space in output
+        }
+    }
+}
