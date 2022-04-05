89f3748186e3215f70a9194309c3a0f8caf21e65
Hamberfim <adhamlingithub@gmail.com>
2022-04-04 19:07:20 -0500

    added simple action controller

---
+++ _01_mvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index(string name = "Guest", int ID = -1)
+        {
+            return ($"Hello {name}, ID: {ID}.");
+        }
+
+        //Requires using System.Text.Encodings.Web;
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name = "Guest", int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"This is the Welcome action method...Hello {name}, NumTimes is: {numTimes}.");
+        }
+
+
+        // Requires using System.Text.Encodings.Web;
+        // Get /HelloWorld/BeGone/
+        public string BeGone(string name = "Guest", int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"This is the BeGone action method...Hello {name}, NumTimes is: {numTimes}.");
+        }
+    }
+}
