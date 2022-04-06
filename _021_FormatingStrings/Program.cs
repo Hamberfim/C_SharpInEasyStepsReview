d2760f8f3b301b39cadc4223dbc63ffe49387e48
Anthony <adhamlingithub@gmail.com>
2022-04-06 12:41:56 -0500

    string formatting

---
+++ _021_FormatingStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _021_FormatingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "String Formatting";
+
+            Console.WriteLine();  // space in output
+        }
+    }
+}
