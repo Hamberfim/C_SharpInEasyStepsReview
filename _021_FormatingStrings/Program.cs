24bfc4b2a4fb91a34ca4a63af5c3d329dc306d87
Anthony <adhamlingithub@gmail.com>
2022-04-06 13:01:58 -0500

    string formatting

--- _021_FormatingStrings/Program.cs
+++ _021_FormatingStrings/Program.cs
@@ -8,14 +8,26 @@ namespace _021_FormatingStrings
         {
             Console.Title = "String Formatting";
             double numericAmount = 25000;
+            double pi = 3.14159265359;
 
             // concat String "Format" method as currency example 
             Console.WriteLine("String ToString 'Format' as Currency : " + numericAmount.ToString("C"));
-            // String "Format" method as currency using placeholders
-            Console.WriteLine("String 'Format' as Currency using placeholder: {0:C}" + numericAmount);
+            Console.WriteLine("String ToString 'Format' as Fixed Point (11 decimal places): " + pi.ToString("F11"));
+            Console.WriteLine();  // space in output
+
+            // String "Format" method as currency using placeholders/position values
+            Console.WriteLine("String 'Format' as Currency using placeholder: {0:C}", numericAmount);
+            Console.WriteLine("String 'Format' as Fixed Point using placeholder: {0:F4}", pi);
+            Console.WriteLine("String 'Format' using placeholder (multiple items): {0:F4}, {1:C}", pi, numericAmount);
+            Console.WriteLine();  // space in output
+
             // String "Format" method as currency using string interpolation
             Console.WriteLine($"String 'Format' method as currency using string interpolation {numericAmount:C}");
 
+            // other numeric format
+            Console.WriteLine($"General format: {numericAmount:G}");
+            Console.WriteLine($"Number format: {numericAmount:N}");
+            Console.WriteLine($"Fixed Point format: {numericAmount:F}");
 
 
             Console.WriteLine();  // space in output
