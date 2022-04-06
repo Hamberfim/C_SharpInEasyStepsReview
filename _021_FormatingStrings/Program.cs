cf58f6f67db6a4a97e4a68ec808aaa66d6741f2c
Anthony <adhamlingithub@gmail.com>
2022-04-06 12:48:05 -0500

    string formatting

--- _021_FormatingStrings/Program.cs
+++ _021_FormatingStrings/Program.cs
@@ -7,8 +7,19 @@ namespace _021_FormatingStrings
         static void Main(string[] args)
         {
             Console.Title = "String Formatting";
+            double numericAmount = 25000;
+
+            // concat String "Format" method as currency example 
+            Console.WriteLine("String ToString format as Currency : " + numericAmount.ToString("C"));
+            // using placeholders
+            Console.WriteLine("String format as Currency using placeholder: {0:C}" + numericAmount);
+
+
 
             Console.WriteLine();  // space in output
+
+
+
         }
     }
 }
