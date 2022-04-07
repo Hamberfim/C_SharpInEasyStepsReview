bc2d7cc473e840a0be2d17c6acdad8e3b8786854
Anthony <adhamlingithub@gmail.com>
2022-04-07 13:13:58 -0500

    write text file to sys

--- _021_WriteFileToSys/Program.cs
+++ _021_WriteFileToSys/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _021_WriteFileToSys
 {
@@ -7,6 +8,22 @@ namespace _021_WriteFileToSys
         static void Main(string[] args)
         {
             Console.Title = "Writting a text file to the system.";
+
+            // set local path to dir and file
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sourceFile = Path.Combine(currentDirectory, @"..\..\..\sourceDocs\fileToReadWrite.txt");
+            string getFilePath = Path.GetFullPath(sourceFile);
+            // check for file at path location - display path to file - Found/Not Found
+            if (File.Exists(getFilePath))
+            {
+                Console.WriteLine($"The file exists at: {getFilePath}.");
+            }
+            else
+            {
+                Console.WriteLine($"File NOT FOUND at: {getFilePath}.");
+            }
+
+
 
             Console.WriteLine();  // Space in output
         }
