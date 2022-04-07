4fe3302af2a4c1c0eb35e434bb7dc011873e71da
Anthony <adhamlingithub@gmail.com>
2022-04-07 13:02:12 -0500

    write fiel to sys

---
+++ _021_WriteFileToSys/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _021_WriteFileToSys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Writting a file to the system.";
+
+            Console.WriteLine();  // Space in output
+        }
+    }
+}
