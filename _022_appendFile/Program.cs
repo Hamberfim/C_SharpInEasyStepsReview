53a0760af7159dd159e3a10d489de1024d284e91
Anthony <adhamlingithub@gmail.com>
2022-04-11 09:38:09 -0500

    append file setup

--- _022_appendFile/Program.cs
+++ _022_appendFile/Program.cs
@@ -9,26 +9,26 @@ namespace _022_appendFile
         {
             Console.Title = "Append/Write file";
 
-            string[] wendallBerry = new string[]
-            {
-                "\tThe Peace of Wild Things",
-                "\tby Wendall Berry"
-            };
+            string wendallBerry = "\tThe Peace of Wild Things";
+            wendallBerry += "\tby Wendall Berry";
 
-            string appendBerry = "\r\n";  // carrage return and new WriteLine
-            appendBerry += "\r\nWhen despair for the world grows in me";
-            appendBerry += "\r\nand I wake in the night at the least sound";
-            appendBerry += "\r\nin fear of what my life and my children’s lives may be,";
-            appendBerry += "\r\nrests in his beauty on the water, and the great heron feeds.";
-            appendBerry += "\r\n";
-            appendBerry += "\r\nI come into the peace of wild things";
-            appendBerry += "\r\nwho do not tax their lives with forethought of grief.";
-            appendBerry += "\r\nI come into the presence of still water.";
-            appendBerry += "\r\n";
-            appendBerry += "\r\nAnd I feel above me the day-blind stars waiting with their light. ";
-            appendBerry += "\r\nFor a time I rest in the grace of the world, and am free.";
-            appendBerry += "\r\n";
-            appendBerry += "\r\nCopyright (c) 2012 by Wendell Berry";
+            // appending content
+            string[] appendBerry = new string[]
+            {   "\r\n",
+                "\r\nWhen despair for the world grows in me",
+                "\r\nand I wake in the night at the least sound",
+                "\r\nin fear of what my life and my children’s lives may be,",
+                "\r\nrests in his beauty on the water, and the great heron feeds.",
+                "\r\n",
+                "\r\nI come into the peace of wild things",
+                "\r\nwho do not tax their lives with forethought of grief.",
+                "\r\nI come into the presence of still water.",
+                "\r\n",
+                "\r\nAnd I feel above me the day-blind stars waiting with their light. ",
+                "\r\nFor a time I rest in the grace of the world, and am free.",
+                "\r\n",
+                "\r\nCopyright (c) 2012 by Wendell Berry."
+            };
 
             // set path to file
             string currentDir = AppDomain.CurrentDomain.BaseDirectory;
@@ -44,8 +44,19 @@ namespace _022_appendFile
             {
                 Console.WriteLine($"File Not Found at {getFilePath}.");
             }
-
             Console.WriteLine();  //  Space in output
+
+            // try/catch write 'title and by'
+            try
+            {
+                File.WriteAllText(getFilePath, wendallBerry);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
         }
     }
 }
