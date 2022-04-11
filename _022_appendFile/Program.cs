5e703b9984277d2cee960ebbd9b0c11769de6669
Anthony <adhamlingithub@gmail.com>
2022-04-11 10:44:04 -0500

    clean

--- _022_appendFile/Program.cs
+++ _022_appendFile/Program.cs
@@ -9,33 +9,33 @@ namespace _022_appendFile
         {
             Console.Title = "Append/Write file";
 
-            string wendallBerry = "\tThe Peace of Wild Things";
-            wendallBerry += "\r\n\tby Wendall Berry";
+            string[] wendallBerryStrArr = new string[]
+            {
+                "\tThe Peace of Wild Things",
+                "\tby Wendall Berry"
+            };
 
             // appending content
-            string[] appendBerry = new string[]
-            {
-                "",
-                "",
-                "...apended content...",
-                "",
-                "",
-                "The Peace of Wild Things by Wendall Berry",
-                "",
-                "When despair for the world grows in me",
-                "and I wake in the night at the least sound",
-                "in fear of what my life and my children’s lives may be,",
-                "rests in his beauty on the water, and the great heron feeds.",
-                "",
-                "I come into the peace of wild things",
-                "who do not tax their lives with forethought of grief.",
-                "I come into the presence of still water.",
-                "",
-                "And I feel above me the day-blind stars waiting with their light. ",
-                "For a time I rest in the grace of the world, and am free.",
-                "",
-                "Copyright (c) 2012 by Wendell Berry."
-            };
+            string appendBerry = "";
+            appendBerry += "\r\n";
+            appendBerry += "\r\n...apended content...";
+            appendBerry += "\r\n";
+            appendBerry += "\r\n";
+            appendBerry += "\r\n\tThe Peace of Wild Things by Wendall Berry";
+            appendBerry += "\r\n";
+            appendBerry += "\r\n\tWhen despair for the world grows in me";
+            appendBerry += "\r\n\tand I wake in the night at the least sound";
+            appendBerry += "\r\n\tin fear of what my life and my children’s lives may be,";
+            appendBerry += "\r\n\trests in his beauty on the water, and the great heron feeds.";
+            appendBerry += "\r\n";
+            appendBerry += "\r\n\tI come into the peace of wild things";
+            appendBerry += "\r\n\twho do not tax their lives with forethought of grief.";
+            appendBerry += "\r\n\tI come into the presence of still water.";
+            appendBerry += "\r\n";
+            appendBerry += "\r\n\tAnd I feel above me the day-blind stars waiting with their light. ";
+            appendBerry += "\r\n\tFor a time I rest in the grace of the world, and am free.";
+            appendBerry += "\r\n";
+            appendBerry += "\r\n\tCopyright (c) 2012 by Wendell Berry.";
 
             // set path to file
             string currentDir = AppDomain.CurrentDomain.BaseDirectory;
@@ -45,12 +45,10 @@ namespace _022_appendFile
             // test if file exists only
             if (File.Exists(getFilePath)) 
             {
-                Console.WriteLine($"The file exists at: {getFilePath}.");
+                Console.WriteLine($"Appending file at: {getFilePath}.");
 
-                // Append string array (WriteAllLines) content to file
-
-                File.WriteAllLines(getFilePath, appendBerry);  // write string array lines
-                Console.WriteLine($"Appended content to file at: {getFilePath}");
+                // Append write string (WriteAllText) 'title and by'
+                File.AppendAllText(getFilePath, appendBerry);  // write string lines
                 // read file and output contents
                 string[] lines = File.ReadAllLines(getFilePath);
                 Console.WriteLine(" === File Output === ");
@@ -65,11 +63,11 @@ namespace _022_appendFile
             {
                 Console.WriteLine($"File Not Found at {getFilePath}.");
 
-                // try/catch write string (WriteAllText) 'title and by'
+                // try/catch string array (WriteAllLines) content to file
                 try
                 {
-                    File.WriteAllText(getFilePath, wendallBerry);  // writes string text
-                    Console.WriteLine($"Title and author written to file at: {getFilePath}");
+                    File.WriteAllLines(getFilePath, wendallBerryStrArr);  // writes string array text
+                    Console.WriteLine($"Title and author written to new file at: {getFilePath}");
                     // read file and output contents
                     string[] lines = File.ReadAllLines(getFilePath);
                     Console.WriteLine(" === File Output === ");
