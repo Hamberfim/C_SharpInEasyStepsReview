358f4086260ebea53d8588197ac5464cf2217804
Anthony <adhamlingithub@gmail.com>
2022-04-11 09:10:07 -0500

    append file setup

---
+++ _022_appendFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace _022_appendFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Append/Write file";
+
+            // find and set path to file
+            string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+            string sourcePath = Path.Combine(currentDir, @"..\..\..\sourceDir\appendTextFile.txt");
+            string getFilePath = Path.GetFullPath(sourcePath);
+
+
+            Console.WriteLine();  //  Space in output
+        }
+    }
+}
