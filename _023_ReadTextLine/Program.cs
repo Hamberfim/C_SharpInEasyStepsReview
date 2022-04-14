51ac62a7f3d042d927883ed19407776002eadd11
Anthony <adhamlingithub@gmail.com>
2022-04-14 13:43:55 -0500

    setup read text,line

---
+++ _023_ReadTextLine/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace _023_ReadTextLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Reading text and lines";
+
+            // set path to file
+            string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+            string sourcePath = Path.Combine(currentDir, @"..\..\..\sourceDir\ReadTextLine.txt");
+            string getFilePath = Path.GetFullPath(sourcePath);
+
+            if(File.Exists(getFilePath))
+            {
+                Console.WriteLine($"Found file at: {getFilePath}.");
+
+                // try read
+                try
+                {
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine($"Error: {error.Message}");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"File Not Found at {getFilePath}.");
+            }
+
+            Console.WriteLine();  // space in output
+
+        }
+    }
+}
