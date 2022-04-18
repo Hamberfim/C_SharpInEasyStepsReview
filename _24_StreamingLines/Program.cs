cab24cd0bc8b65e95477b68fde5fb3a4387b078a
Anthony <adhamlingithub@gmail.com>
2022-04-18 18:54:06 -0500

    StreamWriter

---
+++ _24_StreamingLines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace _24_StreamingLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Streaming Lines -  StreamWriter - WriteStream";
+
+            // set path to file
+            string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+            string sourcePath = Path.Combine(currentDir, @"..\..\..\sourceDir\StreamWriterDoc.txt");
+            string getFilePath = Path.GetFullPath(sourcePath);
+
+            if(File.Exists(getFilePath))
+            {
+                try
+                {
+                    // StreamWriter name = new StreamWriter(PATH);
+                    // using construct
+                    using (StreamWriter name = new StreamWriter(getFilePath))
+                    {
+
+                    }
+                    Console.WriteLine();  // space in outut
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+            
+        }
+    }
+}
