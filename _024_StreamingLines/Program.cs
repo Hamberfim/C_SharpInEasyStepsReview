30b46d6d048de57b6c213d2e0d4d63f9e57319b9
Anthony <adhamlingithub@gmail.com>
2022-04-19 11:22:59 -0500

    streaWriter using constructor

--- _024_StreamingLines/Program.cs
+++ _024_StreamingLines/Program.cs
@@ -14,34 +14,49 @@ namespace _24_StreamingLines
             string sourcePath = Path.Combine(currentDir, @"..\..\..\sourceDir\StreamWriterDoc.txt");
             string getFilePath = Path.GetFullPath(sourcePath);
 
-            // text to work with... a chapter from Brave_New_World_Aldous_Huxley
-            string[] chapter = new string[]
+            // text to work with... a selection from Brave New World Aldous Huxley
+            string[] brave = new string[]
             {
-                "The door of the lighthouse was ajar.", 
-                "They pushed it open and walked into a shuttered twilight.",
-                "Through an archway on the further side of the room they could see the bottom of the staircase that led up to the higher floors.", 
-                "Just under the crown of the arch dangled a pair of feet."
+                "\tThe door of the lighthouse was ajar.", 
+                "\tThey pushed it open and walked into a shuttered twilight.",
+                "\tThrough an archway on the further side of the room they could see the bottom of the staircase that led up to the higher floors.", 
+                "\tJust under the crown of the arch dangled a pair of feet."
             };
 
+            string attrib = "\r\n\tBrave New World (Aldous Huxley 1931)";
 
-            if(File.Exists(getFilePath))
+
+            if (File.Exists(getFilePath))
             {
                 try
                 {
                     // StreamWriter name = new StreamWriter(PATH);
-                    // using construct
-                    using (StreamWriter name = new StreamWriter(getFilePath))
+                    // using construct - write file
+                    using (StreamWriter writer = new StreamWriter(getFilePath))
                     {
-
+                        foreach (string line in brave)
+                        {
+                            writer.WriteLine(line);
+                        }
                     }
-                    Console.WriteLine();  // space in outut
+
+                    // append
+                    using (StreamWriter writer = new StreamWriter(getFilePath, true))
+                    {
+                        writer.WriteLine(attrib);
+                        Console.WriteLine($"File written and appended: {getFilePath}");
+                    }
+
                 }
                 catch (Exception error)
                 {
                     Console.WriteLine(error.Message);
                 }
             }
-            
+
+
+            Console.WriteLine();  // space in outut
+
         }
     }
 }
