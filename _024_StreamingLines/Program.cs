cf733cceffeea98862001604d8122ffd66c39abf
Anthony <adhamlingithub@gmail.com>
2022-04-19 11:14:24 -0500

    steamWriter

--- _024_StreamingLines/Program.cs
+++ _024_StreamingLines/Program.cs
@@ -14,6 +14,16 @@ namespace _24_StreamingLines
             string sourcePath = Path.Combine(currentDir, @"..\..\..\sourceDir\StreamWriterDoc.txt");
             string getFilePath = Path.GetFullPath(sourcePath);
 
+            // text to work with... a chapter from Brave_New_World_Aldous_Huxley
+            string[] chapter = new string[]
+            {
+                "The door of the lighthouse was ajar.", 
+                "They pushed it open and walked into a shuttered twilight.",
+                "Through an archway on the further side of the room they could see the bottom of the staircase that led up to the higher floors.", 
+                "Just under the crown of the arch dangled a pair of feet."
+            };
+
+
             if(File.Exists(getFilePath))
             {
                 try
