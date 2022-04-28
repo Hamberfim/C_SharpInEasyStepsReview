2a75d4ebe866bfaf7aae3d30cd53543bd1c93761
Anthony Hamlin <adhamlingithub@gmail.com>
2022-04-28 11:29:38 -0500

    OOP

---
+++ _027_EncapData/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _027_EncapData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "OOP -Encapsulating Data";
+
+
+
+
+            Console.WriteLine();  // spaced in output
+        }
+    }
+}
