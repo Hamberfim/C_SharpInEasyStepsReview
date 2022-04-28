6656067796c3605cd95ba7b25903cb5b50fb2753
Anthony Hamlin <adhamlingithub@gmail.com>
2022-04-28 10:56:13 -0500

    solve-catch errors

---
+++ _026_SolveErrors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _026_SolveErrors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Solve Errors";
+
+            bool flag = true;
+            /* IConvertible converted = flag;
+            char letter = converted.ToChar(null); */
+
+            // complile-time error
+            try
+            {
+                IConvertible converted = flag;
+                char letter = converted.ToChar(null);
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine($"{error.GetType()} {error.Message}");
+            }
+
+            // runtime error
+
+
+
+            Console.WriteLine();  // space in output
+        }
+    }
+}
