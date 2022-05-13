1f09f5eac38806958b8172467d43b8c8038d5363
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-13 17:26:12 -0500

    base method -hiding

---
+++ _030_hideBaseMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _030_hideBaseMethods
+{
+    internal class Program
+    {
+        
+        public class Man
+        {
+            public void speak()
+            {
+                Console.Write("Hello");
+            }
+            
+            public void speak(string message)
+            {
+                Console.WriteLine($"{message} !\n");
+            }
+        }
+        
+        // derived class
+        public class Hombre : Man
+        {
+            public new void speak(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine();  // space in output
+        }
+    }
+}
