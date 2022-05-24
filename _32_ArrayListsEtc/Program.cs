440b14ca5e2a2d2802b415524d67942f3bad013e
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-24 12:56:05 -0500

    lists, arrays, etc

---
+++ _32_ArrayListsEtc/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _32_ArrayListsEtc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
